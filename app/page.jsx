4794cedb7192447e3fa6a52405b862e388302de7
Domun <zwolinskid1@gmail.com>
2023-01-21 14:59:43 +0100

    Update 2
    - Navbar dolny
    - Drobne zmiany wizualne
    - main, content, zawartość główna

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       </video>
 
       <header>
-        <div className="up">
+        <div className="header-up">
           <p>
             <a href="https://chat.openai.com">
               Introducing ChatGPT research release
@@ -27,9 +27,47 @@ export default function Home() {
             <Image src="/Arrow_L.png" alt="" width="10" height="10" />
           </p>
         </div>
+        <div className="header-down">
+          <a href="https://openai.com">
+            <Image src="/OpenAI_LL.png" alt="logo" width="120" height="30" />
+          </a>
+          <div className="between"></div>
+          <nav>
+            <div className="nav-bar">
+              <p>
+                <a href="https://openai.com/api/">API</a>
+              </p>
+              <p>
+                <a href="https://openai.com/research/">RESEARCH</a>
+              </p>
+              <p>
+                <a href="https://openai.com/blog/">BLOG</a>
+              </p>
+              <p>
+                <a href="https://openai.com/about/">ABOUT</a>
+              </p>
+            </div>
+          </nav>
+        </div>
       </header>
 
-      <main></main>
+      <main>
+        <div className="conteiner">
+          <div className="content">
+            <h1>Join us in shaping the future of technology.</h1>
+            <div className="buttons">
+              <button className="button">
+                <Image src="/Arrow_BL.png" alt="" width="10" height="10" />
+                WATCH VIDEO
+              </button>
+              <p>
+                <a href="https://openai.com/careers/">VIEW CAREERS</a>
+                <Image src="/Arrow_L.png" alt="" width="20" height="10" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
 
       <footer></footer>
     </>
