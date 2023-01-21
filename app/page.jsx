b522e9acc60b01faf6668f6fbb5bfed717eb6e3d
Domun <zwolinskid1@gmail.com>
2023-01-21 17:00:21 +0100

    Update 4
    - Stopka
    - Naprawione błędy
    - Navbar w stopce

--- app/page.jsx
+++ app/page.jsx
@@ -75,7 +75,123 @@ export default function Home() {
         </div>
       </main>
 
-      <footer></footer>
+      <footer>
+        <div className="foother">
+          <div className="foother-cointeiner">
+            <Image src="/OpenAI_LL.png" alt="logo" width="120" height="30" />
+            <div className="flex-box">
+              <div className="item">
+                <ul>
+                  <p>FEATURED</p>
+                  <li>
+                    <a href="https://openai.com/blog/chatgpt/">ChatGPT</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/dall-e-2/">DALL·E 2</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/blog/whisper/">Whisper</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/alignment/">Alignment</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.fund">Startup Fund</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="item">
+                <ul>
+                  <p>API</p>
+                  <li>
+                    <a href="https://openai.com/api/">Overview</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/api/pricing/">Pricing</a>
+                  </li>
+                  <li>
+                    <a href="https://beta.openai.com/examples/">Examples</a>
+                  </li>
+                  <li>
+                    <a href="https://beta.openai.com/docs/">Docs</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/api/policies/service-terms/">
+                      Terms & Policies
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://status.openai.com">Status</a>
+                  </li>
+                  <li>
+                    <a href="https://beta.openai.com/login/">Log in</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="item">
+                <ul>
+                  <p>BLOG</p>
+                  <li>
+                    <a href="https://openai.com/blog/">Index</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/blog/tags/research/">
+                      Research
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/blog/tags/announcements/">
+                      Announcements
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/blog/tags/events/">Events</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/blog/tags/milestones/">
+                      Milestones
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="item">
+                <ul>
+                  <p>INFORMATION</p>
+                  <li>
+                    <a href="https://openai.com/about/">About Us</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/charter/">Our Charter</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/research/">Our Research</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/publications/">Publications</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/newsroom/">Newsroom</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/careers/">Careers</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="end">
+            <p>
+              <a href="https://openai.com">OpenAI © 2015-2023</a>
+            </p>
+            <p>
+              <a href="https://openai.com/privacy/">Privacy Policy</a>
+            </p>
+            <p>
+              <a href="https://openai.com/terms/">Terms of Use</a>
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
